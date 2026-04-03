b15ae555639e3d8c7c71938c7f8c9edad8af170f
millimetr567-rgb <millimetr567-rgb@users.noreply.github.com>
2026-04-03 21:54:15 +0500

    fix: Prisma 7 compatibility - removed URL from schema and added to constructor

--- src/plugins/prisma.js
+++ src/plugins/prisma.js
@@ -2,7 +2,12 @@ const fp = require('fastify-plugin')
 const { PrismaClient } = require('@prisma/client')
 
 module.exports = fp(async (fastify, opts) => {
-  const prisma = new PrismaClient()
+  const prisma = new PrismaClient({
+    datasource: {
+      url: process.env.DATABASE_URL
+    }
+  })
+
   await prisma.$connect()
 
   fastify.decorate('prisma', prisma)
